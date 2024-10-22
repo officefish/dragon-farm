536628e686f2e2f5e922f90ac54a57702e4a8245
officefish <indicogames@yandex.ru>
2024-10-22 17:09:55 +0300

    farm preview added loader added

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -24,10 +24,19 @@ export const WithLoader:FC<PropsWithChildren<ILoadingProps>> = (props) => {
     const { isLoading, children } = props
 
     return isLoading
-        ? <div className="w-screen h-screen">
+        ? <div className="w-screen h-screen screen">
             
-           <div className="w-full h-full flex items-center justify-center">
-               <div className="loader"></div>
+           <div className="w-full h-full flex flex-col items-center justify-center pb-12">
+               <div className='shop-dialog-title mt-8 uppercase px-2'>Total keys left.</div>
+               <div className="flex flex-row gap-2 items-center justify-center mt-4">
+                <img src="/loader/two-cols.png" alt="two-cols" />
+                <img src="/loader/three-cols.png" alt="three cols 1" />
+                <img src="/loader/three-cols.png" alt="three cols 2" />
+               </div>
+           </div>
+
+           <div className="absolute bottom-12 w-full flex justify-center items-center">
+            <span className="loader-progress"></span>
            </div>
 
         </div>
--- src/pages/farm.tsx
+++ src/pages/farm.tsx
@@ -14,7 +14,26 @@ const Farm: FC = () => {
 
     return (
     <div className='w-full'>
-     
+
+    <div className='shop-dialog-title mt-8 uppercase px-2'>find a Great Prize!</div>
+    <div className='mt-3 shop-dialog-description px-2'>
+      9 Types of Rewards Available
+    </div>
+
+    <div className="w-full mt-8">
+      <div className="grid grid-rows-4 grid-cols-4 gap-3">
+          
+          {Array.from({ length: 16 }).map((_, index) => (
+            <div key={index} className="col-span-1 flex h-20 items-center justify-center">
+              <img className="w-12 h-12" src="farm/chest.png" alt="" />
+            </div>
+          ))}
+        
+        </div>
+      </div>
+      <div className="w-full mt-4 px-2">
+        <img className="w-full" src="farm/row.png" alt="" />
+      </div>
     </div>
    )
 }
